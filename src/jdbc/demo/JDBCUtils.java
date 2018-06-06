3a46bd7e2a4281a6680dfe9002d9077351ebc820
alex <110021341@qq.com>
2018-06-07 00:23:05 +0800

    a

--- src/jdbc/demo/JDBCUtils.java
+++ src/jdbc/demo/JDBCUtils.java
@@ -1,17 +1,47 @@
 package jdbc.demo;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class JDBCUtils {
 	
-	private static Connection conn;
+	private String conf;
+	private static String DRIVER;
+	private static String URL;
+	private static String USERNAME;
+	private static String PASSWORD;
+	
 	
 	static {
+		
+		getConf();
+//		System.out.println(DRIVER);
+//		System.out.println(URL);
+//		System.out.println(USERNAME);
+//		System.out.println(PASSWORD);
+		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName(DRIVER);
 		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private static void getConf() {
+		InputStream in = JDBCUtils.class.getClassLoader().getResourceAsStream("conf.properties");
+		Properties pro = new Properties();
+		try {
+			pro.load(in);
+			DRIVER = pro.getProperty("DRIVER");
+			URL = pro.getProperty("URL");
+			USERNAME = pro.getProperty("USERNAME");
+			PASSWORD = pro.getProperty("PASSWORD");
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -21,7 +51,7 @@ public class JDBCUtils {
 		Connection conn = null;
 		
 		try {
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","Aa123456!");
+			conn = DriverManager.getConnection(URL,USERNAME,PASSWORD);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
--- src/jdbc/demo/Test1.java
+++ src/jdbc/demo/Test1.java
@@ -3,28 +3,42 @@ package jdbc.demo;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.Test;
 
 public class Test1 {
-
+	/*
+	 * ºÚµ•≤È—Ø
+	*/
 	public static void main(String[] args) throws Exception {
-		
-		
-		
-		Connection conn = JDBCUtils.getConn();
-		
-		
-		String sql = "select * from product";
-		
-		PreparedStatement prestmt = conn.prepareStatement(sql);
-		
-		ResultSet set = prestmt.executeQuery();
-		
-		while(set.next()) {
-			System.out.println(set.getInt("pid") + ":" + set.getString("pname"));
-		}
-		
-		
-		
+//		Connection conn = JDBCUtils.getConn();
 	}
+	
+	@Test
+	public void getAll() {
+		Connection conn = JDBCUtils.getConn();
+		String sql = "select * from product";
+		ResultSet set = null;
+		PreparedStatement prestmt;
+		try {
+			prestmt = conn.prepareStatement(sql);
+			set = prestmt.executeQuery();
+			while(set.next()) {
+				System.out.println(set.getInt("pid") + ":" + set.getString("pname") + "-" + set.getDouble("price"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				set.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
 
 }
